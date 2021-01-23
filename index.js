f0f7499afd0981889ab0ff6320485ba5a7b40726
Mune <tongtruongthinh95tv@gmail.com>
2021-01-23 20:41:54 +0700

    cleanup code

--- index.js
+++ index.js
@@ -1,11 +1,12 @@
-$(".works").click(() => {
-	$("*:not(.header)").fadeOut(3000, () => {
-		window.location.href("/works");
+$(document).ready(function () {
+	$("body").hide().fadeIn(1000);
+	$("a").click(function (e) {
+		e.preventDefault();
+		$link = $(this).attr("href");
+		$("body").fadeOut(1000, function () {
+			window.location = $link;
+		});
 	});
 });
 
-$(".links").click(() => {
-	$("*:not(.header)").fadeOut(3000, () => {
-		window.location.href("/links");
-	});
-});
+(() => {})();
